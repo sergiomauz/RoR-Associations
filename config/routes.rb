b221218811b3619a2b662458df8d46ed0c0b85eb
sergiomauz <sergio.mauz88@gmail.com>
2020-05-07 11:11:24 -0500

    Added user forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "users#new"
 end
